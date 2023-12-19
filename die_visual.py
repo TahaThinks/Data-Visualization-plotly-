84bc6a06d3dbd7019686c6e587a5076d0f104e4a
Taha Ahmed Abdelazim Hussein <71207531+TahaThinks@users.noreply.github.com>
2023-12-20 01:15:58 +0400

    create x-axis and y-axis values

--- die_visual.py
+++ die_visual.py
@@ -16,3 +16,7 @@ frequencies = []
 for value in range(1,die.num_sides+1):
     frequency = results.count(value)
     frequencies.append(frequency)
+
+# Visualize the results.
+    x_values = list(range(1,die.num_sides+1))
+    data = [Bar(x=x_values, y=frequencies)]
