bb6dc0c918c3068d4aabc893b76d085fa41b8f96
Taha Ahmed Abdelazim Hussein <71207531+TahaThinks@users.noreply.github.com>
2023-12-20 01:22:14 +0400

    Layout Class that specifying config of the graph

--- die_visual.py
+++ die_visual.py
@@ -23,3 +23,5 @@ data = [Bar(x=x_values, y=frequencies)]
 
 x_axis_config = {'title': "Result"}
 y_axis_config = {'title': "Frequency of Result"}
+
+my_layout = Layout(title="Results of rolling one D6 1000 times", xaxis = x_axis_config, yaxis=y_axis_config)
