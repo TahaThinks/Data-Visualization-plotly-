f9ce3193afddab67d553dc695bf4b9c951694f1a
Taha Ahmed Abdelazim Hussein <71207531+TahaThinks@users.noreply.github.com>
2023-12-20 00:07:54 +0400

    Adding Class Die

--- die.py
+++ die.py
@@ -1,1 +1,8 @@
 from random import randint
+
+class Die:
+    """A Class represeenting a single Dice"""
+
+    def __init__(self, num_sides=6):
+        """Assume a six-sided die."""
+        self.num_sides = num_sides
